dc6aff7618db329ae8cdba4f5bd9ee21e24bc28c
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-03-23 00:22:54 +0530

    style : changed width of div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { LandingPage } from "./pages";
+import { Productlisting } from "./pages/ProductLists/Productlisting";
 
 function App() {
   return (
     <div className="App">
-    <LandingPage/>  
+    <LandingPage/>
+    <Productlisting/>  
     </div>
   );
 }
